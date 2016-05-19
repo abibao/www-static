db36b93c633cd5351d71e3e057544398aa5caef5
boitaumail <boitaumail@yahoo.fr>
2016-05-19 15:38:01 +0200

    Analytics (part 02)

--- src/templates/assets/js/main.js
+++ src/templates/assets/js/main.js
@@ -1,47 +1,33 @@
 	//Event quand la homepage converti
 	$('#howto').on('click', function (e) {
 			//On pousse un évènement pour Google Analytics
-	        var subscribe_page = "Page : " + window.location.pathname; //On récupère le nom de la page en cours qui à poussé au subscribe.
+	        var subscribe_page = "Page " + window.location.pathname; //On récupère le nom de la page en cours qui à poussé au subscribe.
 	        alert(subscribe_page);
 			ga('send', {
 						  hitType: 'event',
 						  eventCategory: 'Navigation',
 						  eventAction: 'GoToFAQ',
-						  eventLabel: 'From_'+subscribe_page
+						  eventLabel: 'Header From : '+subscribe_page
 						});
 		});
 	//Event quand la homepage converti
 	$('#cta_header').on('click', function (e) {
 			//On pousse un évènement pour Google Analytics
-	        var subscribe_page = "Page : " + window.location.pathname; //On récupère le nom de la page en cours qui à poussé au subscribe.
+	        var subscribe_page = "Page " + window.location.pathname; //On récupère le nom de la page en cours qui à poussé au subscribe.
 	        alert(subscribe_page);
 			ga('send', {
 						  hitType: 'event',
 						  eventCategory: 'Conversion',
 						  eventAction: 'subscribe',
-						  eventLabel: 'Header_From_'+subscribe_page
+						  eventLabel: 'Header From : '+subscribe_page
 						});
 		});
 	//Event quand la homepage converti
-/*	$('#cta_button').on('click', function (e) {
-			//On pousse un évènement pour Google Analytics
-	        var subscribe_page = "Page : " + window.location.pathname; //On récupère le nom de la page en cours qui à poussé au subscribe.
-	        alert(subscribe_page);
-			ga('send', {
-						  hitType: 'event',
-						  eventCategory: 'Conversion',
-						  eventAction: 'subscribe',
-						  eventLabel: 'Button_From_'+subscribe_page
-						});
-		});*/
-
-
 	$("#mail-catcher").submit(function(event) {
-	        alert("submit ! "+subscribe_page);
 			ga('send', {
 						  hitType: 'event',
 						  eventCategory: 'Conversion',
 						  eventAction: 'subscribe',
-						  eventLabel: 'Button_From_'+subscribe_page
+						  eventLabel: 'Form homepage'
 						});
 	});
